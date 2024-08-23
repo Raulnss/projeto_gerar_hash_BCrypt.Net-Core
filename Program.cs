ef06edfa5e5b3d0d95a4a40ef656f3b830a4c10f
Raulnss <raulnsantosdesousa@gmail.com>
2024-08-22 21:18:47 -0300

    hash

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BCrypt.Net;
+
+namespace BCryptNetCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Coloque a senha: ");
+            string senha = Console.ReadLine();
+            string hashSenha = BCrypt.Net.BCrypt.HashPassword(senha);
+            Console.WriteLine($"Hash gerado: {hashSenha}");
+            Console.ReadKey();
+            Console.WriteLine("coloque a senha");
+            string n1 = Console.ReadLine();
+            if (BCrypt.Net.BCrypt.Verify(n1, hashSenha))
+            {
+                Console.WriteLine(" senha correta");
+            }
+            else
+            {
+                Console.WriteLine(" senha incorreta");
+            }
+            Console.ReadKey();
+        }
+    }
+}
